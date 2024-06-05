d3727a63d195606bdd68d62682c9bd8dbee49577
GianlucaZan <gianluca.zanq@gmail.com>
2024-06-05 16:13:45 -0300

    Components #4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
 
@@ -18,6 +18,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1, 2)}</p>
       <img src={url} alt='Minha Imagem'/>
+      <HelloWorld />
     </div>
   );
 }
